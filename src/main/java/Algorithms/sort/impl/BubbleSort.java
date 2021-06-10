311e73626d8aabccb103ce690aa30a1981e84644
Tenfi <405013286@qq.com>
2021-06-10 08:34:22 +0800

    修改 冒泡排序实现

--- src/main/java/Algorithms/sort/impl/BubbleSort.java
+++ src/main/java/Algorithms/sort/impl/BubbleSort.java
@@ -14,14 +14,12 @@ public class BubbleSort extends AbstractSort {
             return;
         }
 
-        int index = list.length;
-        while (index != 1) {
-            for (int i = 0; i < index - 1; i++) {
-                if (list[i] > list[i + 1]) {
-                    swap(list, i, i + 1);
+        for (int i = list.length; i > 1; i--) {
+            for (int j = 0; j < i-1; j++) {
+                if (list[j] > list[j + 1]) {
+                    swap(list, j, j + 1);
                 }
             }
-            index--;
         }
     }
 }

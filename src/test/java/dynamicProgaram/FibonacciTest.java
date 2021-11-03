a9c905b6ad920569eb49f46be28e0e2d9fc7afd4
Tenfi <405013286@qq.com>
2021-11-03 22:46:22 +0800

    新增 斐波那契数优化练习

--- src/test/java/dynamicProgaram/FibonacciTest.java
+++ src/test/java/dynamicProgaram/FibonacciTest.java
@@ -3,32 +3,44 @@ package dynamicProgaram;
 import org.junit.Test;
 
 public class FibonacciTest {
-    public int calcu(int num) {
-        if (num == 0) {
-            return 0;
-        }
-        if (num == 1) {
-            return 1;
-        }
-        return calcu(num - 1) + calcu(num - 2);
-    }
-
-    public int calcuRecord(int num) {
-        int[] ints = new int[num];
-        ints[0] = 0;
-        ints[1] = 1;
-
-        for (int i = 2; i < num; i++) {
-            ints[i] = ints[i-1] + ints[i-2];
-        }
-        return ints[num-1] + ints[num-2];
-    }
-
 
     @Test
     public void test() {
-//        System.out.println(calcu(10));
-        System.out.println(calcuRecord(30));
+        System.out.println(fib(30));
+        System.out.println(newFib1(30));
+        System.out.println(newFib2(30));
     }
 
+
+    public int fib(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 2 || num == 1) {
+            return 1;
+        }
+        return fib(num - 1) + fib(num - 2);
+    }
+
+    public int newFib1(int num) {
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[num];
+    }
+
+    public int newFib2(int num) {
+        int pre = 0;
+        int cur = 1;
+        int next = 1;
+        for (int i = 2; i <= num; i++) {
+            next = pre + cur;
+            pre = cur;
+            cur = next;
+        }
+        return next;
+    }
 }

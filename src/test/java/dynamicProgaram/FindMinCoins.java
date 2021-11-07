6cc7856059af28f6d22c93a90cb7cc1009518ee3
Tenfi <405013286@qq.com>
2021-11-07 09:03:34 +0800

    新增 凑最少硬币

---
+++ src/test/java/dynamicProgaram/FindMinCoins.java
@@ -0,0 +1,48 @@
+package dynamicProgaram;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FindMinCoins {
+    // 从coins中，找最小数量的硬币满足target
+    public static int findMinNumCoins(Integer[] coins, int target) {
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int shang = 0;
+        int yu = 1;
+        for (int coin : coins) {
+            if (yu == 0) {
+                break;
+            }
+            shang += target / coin;
+            yu = target % coin;
+            target = yu;
+        }
+        return shang;
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = new Integer[]{1,5,10};
+        System.out.println(findMinNumCoins(nums,36));
+        System.out.println(dp(nums,36));
+    }
+
+
+    public static int dp(Integer[] coins,int num){
+        if (num==0){
+            return 0;
+        }
+        if (num<0){
+            return -1;
+        }
+        int res = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int sub = dp(coins,num-coin);
+            if (sub==-1){
+                continue;
+            }
+            res = Math.min(res,sub+1);
+        }
+        return res == Integer.MAX_VALUE? -1:res;
+    }
+}

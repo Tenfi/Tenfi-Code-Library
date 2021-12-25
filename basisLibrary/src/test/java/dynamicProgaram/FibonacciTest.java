28daa1ee5cb02034404de5b5394eef36ad35388c
Tenfi <405013286@qq.com>
2021-12-25 08:24:36 +0800

    修改 斐波那契数计算

--- basisLibrary/src/test/java/dynamicProgaram/FibonacciTest.java
+++ basisLibrary/src/test/java/dynamicProgaram/FibonacciTest.java
@@ -7,8 +7,8 @@ public class FibonacciTest {
     @Test
     public void test() {
         System.out.println(fib(30));
-        System.out.println(newFib1(30));
-        System.out.println(newFib2(30));
+        System.out.println(newFib1(0));
+        System.out.println(newFib2(0));
     }
 
 
@@ -23,6 +23,12 @@ public class FibonacciTest {
     }
 
     public int newFib1(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
         int[] dp = new int[num + 1];
         dp[0] = 0;
         dp[1] = 1;
@@ -33,14 +39,22 @@ public class FibonacciTest {
     }
 
     public int newFib2(int num) {
-        int pre = 0;
-        int cur = 1;
-        int next = 1;
-        for (int i = 2; i <= num; i++) {
-            next = pre + cur;
-            pre = cur;
-            cur = next;
+        if (num == 0) {
+            return 0;
         }
-        return next;
+        if (num == 1) {
+            return 1;
+        }
+        int prePreNum = 0;
+        int preNum = 1;
+        int curNum = 1;
+        for (int i = 2; i < num + 1; i++) {
+            curNum = prePreNum + preNum;
+            prePreNum = preNum;
+            preNum = curNum;
+        }
+        return curNum;
     }
+
+
 }
